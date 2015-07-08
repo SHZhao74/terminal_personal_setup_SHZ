81f1f8d20b7ab4a26b685528bda3914727a0a56d
SHZhao74 <j5201314peter@gmail.com>
2015-07-08 13:51:42 +0800

    first commit

---
+++ reInstall_System_Packages.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt-get install vim
+sudo apt-get install cmake
+sudo apt-get install g++
+sudo apt-get install ssh
+sudo apt-get install git
+
+echo -e "\033[1;33;40mInstall ZSH!!!\033[0m\n"
+sudo apt-get install zsh
+sudo wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh 
+chsh -s /bin/zsh
+
+
+echo -e "\033[1;33;40mSet auto ls command after cd command\033[0m\n"
+vim ~/.zshrcfunction -c '$put=\"\nchpwd() {\n    emulate -L zsh\n    ls \n}\"' -c 'wq'
+
+echo -e "\033[1;33;40mSet vim tab size = 4 \033[0m\n"
+vim ~/.vimrc -c '$put=\"tabstop=4\"'
+
+#echo "Install AMD SDK!!!!!\n"
+#sudo ./AMD-APP-SDK-v2.9-1.599.381-GA-linux64.sh
+
+
