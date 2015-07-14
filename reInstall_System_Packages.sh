e874024b4520a982699e509b7441cec4f44f8d61
SHZhao74 <j5201314peter@gmail.com>
2015-07-14 18:27:20 +0800

    add install svn bug fix

--- reInstall_System_Packages.sh
+++ reInstall_System_Packages.sh
@@ -3,7 +3,7 @@ sudo apt-get install vim
 sudo apt-get install cmake
 sudo apt-get install g++
 sudo apt-get install ssh
-sudo apt-get install git
+sudo apt-get install git subversion
 
 echo -e "\033[1;33;40mInstall ZSH!!!\033[0m\n"
 sudo apt-get install zsh
@@ -15,7 +15,7 @@ echo -e "\033[1;33;40mSet auto ls command after cd command\033[0m\n"
 vim ~/.zshrcfunction -c '$put=\"\nchpwd() {\n    emulate -L zsh\n    ls \n}\"' -c 'wq'
 
 echo -e "\033[1;33;40mSet vim tab size = 4 \033[0m\n"
-vim ~/.vimrc -c '$put=\"tabstop=4\"'
+vim ~/.vimrc -c '$put=\"set tabstop=4\"' -c 'wq'
 
 #echo "Install AMD SDK!!!!!\n"
 #sudo ./AMD-APP-SDK-v2.9-1.599.381-GA-linux64.sh
