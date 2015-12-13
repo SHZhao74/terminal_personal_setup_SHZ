4e4b9f7fdfa3b33296d57d909824d6f8ec7333b6
j5201314peter@gmail.com <newslab@hao.novalocal>
2015-12-13 19:14:56 +0000

    will clone Powerline fonts, in order to change zsh them

--- reInstall_System_Packages.sh
+++ reInstall_System_Packages.sh
@@ -2,10 +2,10 @@
 sudo apt-get update
 sudo apt-get install vim cmake g++ ssh git subversion
 
-echo  "\033[1;33;40mInstall ZSH and Oh-My-ZSH!!!\033[0m\n"
+echo  "\033[1;33;40mInstall ZSH, Oh-My-ZSH, and Powerline Fonts\n Remenber install Menlo for powerline font!!!\033[0m\n"
 sudo apt-get install zsh
 sudo wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh 
-
+git clone https://github.com/supermarin/powerline-fonts
 
 echo  "\033[1;33;40mSet auto ls command after cd command\033[0m\n"
 vim ~/.zshrc -c '$put=\"\nchpwd() {\n    emulate -L zsh\n    ls \n}\"' -c 'wq'
